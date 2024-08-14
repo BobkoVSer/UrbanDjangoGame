fe8732185d9f8daad5782167b5709994ecf95891
NewDjangoProject <156786477+BobkoVSer@users.noreply.github.com>
2024-08-14 20:05:52 +0700

    Add files via upload

--- models.py
+++ models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__ (self):
+        return self.title
+
 class Buyer(models.Model):
     name = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=6, decimal_places=2)
